2312ea302a6664648ebca5a77826060681daa280
Adrien Bricchi <adrien.bricchi@adullact-projet.coop>
2015-11-02 19:03:04 +0100

    Click watched on settings elements

--- iParapheur/SettingsTableViewController.h
+++ iParapheur/SettingsTableViewController.h
@@ -1,12 +1,9 @@
-//
-// Created by Adrien Bricchi on 02/11/2015.
-//
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 
-@interface SettingsTableViewController : UITableViewController
+@interface SettingsTableViewController : UITableViewController <UITableViewDataSource>
 
 
 
--- iParapheur/SettingsTableViewController.m
+++ iParapheur/SettingsTableViewController.m
@@ -1,25 +1,69 @@
-//
-// Created by Adrien Bricchi on 02/11/2015.
-//
 
 #import "SettingsTableViewController.h"
 
 
 @implementation SettingsTableViewController {
 	IBOutlet UIBarButtonItem *backButton;
+	NSDictionary *rowsDictionary;
 }
 
 - (void)viewDidLoad {
 	NSLog(@"View Loaded : SettingsTableViewController");
 
 	backButton.target = self;
-	backButton.action = @selector(onBackButtonClicked:);
+	backButton.action = @selector(onBackButtonClicked);
+
+	rowsDictionary = @{
+			@"Général" : @[@"Comptes", @"Certificats"],
+			@"À propos" : @[@"Informations légales", @"Licences tierces"]
+	};
+}
+
+
+#pragma mark - UITableViewDatasource
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+	return rowsDictionary.count;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section {
+
+	NSArray *sectionArray = rowsDictionary[rowsDictionary.allKeys[(NSUInteger) section]];
+	return sectionArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+	static NSString *MyIdentifier = @"SettingCell";
+	UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:MyIdentifier];
+
+	if (cell == nil)
+		cell = [[UITableViewCell new] initWithFrame:CGRectZero
+		                            reuseIdentifier:MyIdentifier];
+
+	NSArray *sectionArray = rowsDictionary[rowsDictionary.allKeys[(NSUInteger) indexPath.section]];
+	cell.textLabel.text = sectionArray[(NSUInteger) indexPath.row];
+
+	return cell;
+}
+
+- (void)      tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+	NSArray *sectionArray = rowsDictionary[rowsDictionary.allKeys[(NSUInteger) indexPath.section]];
+	NSLog(@"Adrien - selected : %@", sectionArray[(NSUInteger) indexPath.row]);
 }
 
 
 #pragma mark - Buttons callbacks
 
-- (void)onBackButtonClicked:(id)sender {
+- (void)onBackButtonClicked {
 
 	[self.presentingViewController dismissViewControllerAnimated:YES
 	                                                  completion:nil];
