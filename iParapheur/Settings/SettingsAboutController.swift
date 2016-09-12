d28c5d2e8966a23696e9f01595bac384f9cd870c
Adrien Bricchi <adrien.bricchi@adullact-projet.coop>
2016-09-12 15:49:36 +0200

    About panel

--- iParapheur/Settings/SettingsAboutController.swift
+++ iParapheur/Settings/SettingsAboutController.swift
@@ -38,26 +38,12 @@ import UIKit
 
 class SettingsAboutController: UIViewController {
 	
+	@IBOutlet var versionLabel: UILabel!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		
+		let appVersionString: String = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as! String
+		versionLabel.text = versionLabel.text?.stringByReplacingOccurrencesOfString(":version:", withString: appVersionString)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
