11ba9c62c401e0a6f42f7dc89967e5915d009421
Adrien Bricchi <adrien.bricchi@adullact-projet.coop>
2017-11-23 10:30:35 +0100

    ipacert Open With...

--- iParapheur/UI/RGMasterViewController.m
+++ iParapheur/UI/RGMasterViewController.m
@@ -1,8 +1,7 @@
 /*
- * Copyright 2012-2016, Adullact-Projet.
- * Contributors : SKROBS (2012)
+ * Copyright 2012-2017, Libriciel SCOP.
  *
- * contact@adullact-projet.coop
+ * contact@libriciel.coop
  *
  * This software is a computer program whose purpose is to manage and sign
  * digital documents on an authorized iParapheur.
