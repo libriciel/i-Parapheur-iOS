0381dd1e5b80da3573cf092f8b5e0de2f8b89bc6
Adrien Bricchi <adrien.bricchi@adullact-projet.coop>
2017-11-24 01:46:22 +0100

    Licence update

--- iParapheur/Models/ParapheurType.swift
+++ iParapheur/Models/ParapheurType.swift
@@ -1,7 +1,7 @@
 /*
- * Copyright 2012-2016, Adullact-Projet.
+ * Copyright 2012-2017, Libriciel SCOP.
  *
- * contact@adullact-projet.coop
+ * contact@libriciel.coop
  *
  * This software is a computer program whose purpose is to manage and sign
  * digital documents on an authorized iParapheur.
@@ -32,7 +32,6 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
-
 import Foundation
 import Gloss
 
